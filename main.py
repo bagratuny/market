2b241b4267be4f4d222c5950240984d38cb91242
bagratuny <mrarsew@gmail.com>
2018-02-17 01:30:24 +0300

    Rewritten main(), fixed offset and more

--- main.py
+++ main.py
@@ -3,51 +3,51 @@ from time import sleep
 import requests
 from api_links import API_LINKS, COINS
 
-bot_token = API_LINKS.get('bot_token')
-crypto_api = API_LINKS.get('crypto_api')
-btc = COINS.get('btc')
-eth = COINS.get('eth')
+BOT_TOKEN = API_LINKS.get('bot_token')
+COIN_API = API_LINKS.get('crypto_api')
+BTC = COINS.get('btc')
+ETH = COINS.get('eth')
 
 dictionary = json.load(open('dictionary.json'))
 
-def get_last_update():
-    params = {'timeout': 100, 'offset': [-1]}
-    parsed_data = requests.get(bot_token + 'getUpdates', data = params)
+def get_last_update(huy):
+    params = {'timeout': 100, 'offset': huy}
+    parsed_data = requests.get(BOT_TOKEN + 'getUpdates', data = params)
     response = parsed_data.json()
-    return response['result'][-1]
+    return response['result']
 
 def send_message(chat_id, message_text):
     body = {'chat_id': chat_id, 'text': message_text}
-    response = requests.post(bot_token + 'sendMessage', data=body)
+    response = requests.post(BOT_TOKEN + 'sendMessage', data=body)
     return response
 
 def get_coin_price(coin):
-    parsed_data = requests.get(crypto_api + coin + '-usd/')
+    parsed_data = requests.get(COIN_API + coin + '-usd/')
     response = parsed_data.json()
-    return response['ticker']['price']
+    response2 = response['ticker']['price']
+    return str(response2)
 
 def main():
-    last_update = get_last_update()
-    update_id = last_update['update_id']
+    offset = 0
     while True:
-        last_update = get_last_update()
-        message_text = last_update['message']['text']
-        chat_id = last_update['message']['chat']['id']
-        first_name = last_update['message']['from']['first_name']
-        if update_id == last_update['update_id']:
-            if message_text == '/start':
-                send_message(chat_id, first_name + dictionary['start'])
-            elif message_text == '/help':
-                send_message(chat_id, dictionary['help'])
-            elif message_text == '/btc':
-                send_message(chat_id, dictionary['btc'] +str(get_coin_price(btc)))
-            elif message_text == '/eth':
-                send_message(chat_id, dictionary['eth'] +str(get_coin_price(eth)))
-            else:
-                send_message(chat_id, dictionary['error'])
-            print('message: ' + message_text + ', user: ' + first_name)
-            update_id += 1
-            print(update_id)
-        sleep(1)
+        len_updates = get_last_update(offset)
+        if len(len_updates) > 0:
+            for count in len_updates:
+                message_text = count['message']['text']
+                chat_id = count['message']['chat']['id']
+                first_name = count['message']['from']['first_name']
+                if message_text == '/start':
+                    send_message(chat_id, first_name + dictionary['start'])
+                elif message_text == '/help':
+                    send_message(chat_id, dictionary['help'])
+                elif message_text == '/btc':
+                    send_message(chat_id, dictionary['btc'] +str(get_coin_price(BTC)))
+                elif message_text == '/eth':
+                    send_message(chat_id, dictionary['eth'] +str(get_coin_price(ETH)))
+                else:
+                    send_message(chat_id, dictionary['error'])
+                print('message: ' + message_text + ', user: ' + first_name)
+            offset = len_updates[-1]['update_id'] + 1
+        sleep(5)
 if __name__ == '__main__':
     main()
