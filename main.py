38af11476667652320dd51955e51e5dae83986ab
bagratuny <mrarsew@gmail.com>
2018-02-18 00:50:53 +0300

    Optimization, now Bot support more than 300 cryptocurrencies

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 from time import sleep
 import requests
-from api_links import API_LINKS, COINS
+from api_links import API_LINKS
 
 BOT_TOKEN = API_LINKS.get('bot_token')
 CRYPTONATOR_API = API_LINKS.get('cryptonator_api')
@@ -16,13 +16,9 @@ def send_message(chat_id, message_text):
     body = {'chat_id': chat_id, 'text': message_text}
     requests.post(BOT_TOKEN + 'sendMessage', data=body)
 
-def get_coin_price(coin):
-    boolean_test = requests.get(CRYPTONATOR_API + coin + '-usd/')
-    if boolean_test.json()['success'] == True:
-        response = boolean_test.json()
-    else:
-        response = None
-    return response
+def get_coin_rate(coin):
+    response = requests.get(CRYPTONATOR_API + coin + '-usd/')
+    return response.json()
 
 def main():
     offset = 0
@@ -33,23 +29,22 @@ def main():
                 message_text = update['message']['text']
                 chat_id = update['message']['chat']['id']
                 first_name = update['message']['from']['first_name']
-                if not get_coin_price(message_text):
-                    coin_price = 'False'
-                else: 
-                    coin_price = get_coin_price(message_text)['ticker']['price']
-                    coin_name = get_coin_price(message_text)['ticker']['base']
+                if not get_coin_rate(message_text)['success']:
+                    coit_rate = 'False'
+                else:
+                    coin_rate = get_coin_rate(message_text)['ticker']['price']
+                    coin_name = get_coin_rate(message_text)['ticker']['base']
                 if message_text == '/start':
                     send_message(chat_id, first_name + DICT['start'])
                 elif message_text == '/help':
                     send_message(chat_id, DICT['help'])
-                elif get_coin_price(message_text) == None:
+                elif get_coin_rate(message_text) == None:
                     send_message(chat_id, DICT['error'])
                 else:
-                    send_message(chat_id, coin_name + ' rate is: $' + coin_price)
+                    send_message(chat_id, coin_name + ' rate is: $' + coin_rate)
                 print('message: ' + message_text + ', user: ' + first_name)
             offset = len_updates[-1]['update_id'] + 1
         sleep(1)
 
 if __name__ == '__main__':
     main()
- 
